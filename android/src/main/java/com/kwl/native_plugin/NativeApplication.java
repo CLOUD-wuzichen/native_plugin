78693a853d7b16ce2407a3e97af28c0b753849ac
CLOUD-wuzichen <412871942@qq.com>
2019-03-25 16:11:49 +0800

    修复bug

---
+++ android/src/main/java/com/kwl/native_plugin/NativeApplication.java
@@ -0,0 +1,18 @@
+package com.kwl.native_plugin;
+
+import com.hjq.toast.ToastUtils;
+
+import io.flutter.app.FlutterApplication;
+
+/**
+ * Created by wu.zc on 2019/3/25
+ * e-mail : wuzc@szkingdom.com
+ * desc :
+ */
+public class NativeApplication extends FlutterApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
